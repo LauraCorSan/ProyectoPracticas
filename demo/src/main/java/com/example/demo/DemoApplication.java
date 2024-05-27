b82380e20b23e50025ef136ad1fd50aa04d0166a
ALEJANDRO DEL FRESNO <adelfresnomartin@gmail.com>
2024-05-27 13:17:15 +0200

    nuevo proyecto con cambio de dependencias

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo_maven;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoMavenApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoMavenApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo_maven;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoMavenApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
