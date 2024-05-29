b12b7070243ec8025ac6edc712f90b4fbaf7f343
Arturo Yáñez <127496891+arturoyz@users.noreply.github.com>
2024-05-29 10:30:08 +0200

    JPA Alergenos

--- proyecto/src/main/resources/import.sql
+++ proyecto/src/main/resources/import.sql
@@ -3,3 +3,8 @@ INSERT INTO users (create_at, date_of_bith, email, name, password, surname, user
 INSERT INTO users (create_at, date_of_bith, email, name, password, surname, username) VALUES ('2024-05-28', '1980-09-20', 'correo4@example.com', 'Ana', 'contraseña123', 'García', 'ana_g');
 INSERT INTO users (create_at, date_of_bith, email, name, password, surname, username) VALUES ('2024-05-28', '1992-07-05', 'correo5@example.com', 'Luis', 'pass456', 'Martínez', 'luisito');
 INSERT INTO users (create_at, date_of_bith, email, name, password, surname, username) VALUES ('2024-05-28', '2000-01-01', 'correo1@example.com', 'Juan', 'secreto123', 'Pérez', 'juanito');
+INSERT INTO alergens (name) VALUES ('Frutos secos');
+INSERT INTO alergens (name) VALUES ('Brocoli');
+INSERT INTO alergens (name) VALUES ('Marisco');
+INSERT INTO alergens (name) VALUES ('Pescado');
+INSERT INTO alergens (name) VALUES ('Nueces');
